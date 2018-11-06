fad297d318aba948f9edcab53fe68f667357c109
notyourbear <pracenberg@gmail.com>
2018-11-06 14:09:24 -0800

    should work now

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ app.use(compression());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, X-Custom-Header"
   );
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   next();
 });
 
